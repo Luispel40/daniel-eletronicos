fcc993f11615f751f5cee95679891359d3afdfc8
Luispel40 <lfdnpel@gmail.com>
2025-06-23 18:28:37 -0300

    add new category for shop

--- carousel.js
+++ carousel.js
@@ -1,32 +1,39 @@
 let aData = [];
 
-aData = JSON.parse(sessionStorage.getItem('data'));
+aData = JSON.parse(sessionStorage.getItem("data"));
 
-const carousel = document.getElementById('grid-products-2');
-const previousButton = document.getElementById('previous');
-const nextButton = document.getElementById('next');
+const carousel = document.getElementById("grid-products-2");
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
 
 console.log(aData);
 
-carousel.innerHTML = '';
+carousel.innerHTML = "";
 
 let currentSlide = 0;
 
 const createProductsFromCarousel = () => {
-    aData.map((product) => {
-        const productElement = document.createElement('div');
-        productElement.classList.add('product');
-        productElement.innerHTML = `
+  aData.map((product) => {
+    const productElement = document.createElement("div");
+    productElement.classList.add("product");
+    productElement.innerHTML = `
             <img src="${product.image}" alt="${product.name}" />
             <div class="info">
-                <h3 class="title">${product.productName} <span>R$ ${product.value}</span></h3>
+                <h3 class="title">${product.productName} ${
+      product.offer
+        ? `<span>de <s>${product.value}</s> por ${product.offer}</span>`
+        : `<span> ${product.value}</span>`
+    }</h3>
+        </h3>
                 <p>${product.description}</p>
-                <button class="button" onClick="sendToWhatsapp()">Comprar com o vendedor</button>
+                <button >
+          <a href="../product/index.html?id=${product.index}" class="button
+    }">${product.avaliable ? "Ver Mais" : "Em Breve"}</a>
+        </button>
             </div>
         `;
-        carousel.innerHTML += productElement.outerHTML;
-    })
-}
+    carousel.innerHTML += productElement.outerHTML;
+  });
+};
 
 createProductsFromCarousel();
-
